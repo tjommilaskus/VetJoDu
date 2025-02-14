7f536d6e3c99587aee155efe31b742ea1b2eae6f
Tjommilaskus <mps.94@me.com>
2025-02-14 18:42:59 +0100

    Changed color and text size

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
     return (
         <div className="min-h-screen flex items-center justify-center p-5">
-            <h1 className="text-7xl uppercase text-orange-400">Vet Jo Du!</h1>
+            <h1 className="text-9xl uppercase text-orange-400 font-bold">Vet Jo Du!</h1>
         </div>
     );
 }
